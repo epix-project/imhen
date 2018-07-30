d364c2820b46ddb55ee7699123e71acd231aa4fc
Lucie Contamin <contamin.lucie@gmail.com>
2018-07-30 17:50:53 +0200

    Update vignette and documentation

--- R/imhen.R
+++ R/imhen.R
@@ -1,7 +1,7 @@
 #' imhen: Meteorological data for Vietnam.
 #'
 #' The imhen package provides monthly times series for 7 climatic variables in
-#' 67 climatic stations of Vietnam from 1960 to 2015.
+#' 67 climatic stations of Vietnam from 1960 to 2017.
 #'
 #' @docType package
 #' @name imhen
